d58f3e6e299ab274ac09fc02a4b6dd26578980e4
Klaxer77 <goluzin-vanya@mail.ru>
2024-01-31 21:03:13 +0500

    adwdad

--- backend/backend/user/validators.py
+++ backend/backend/user/validators.py
@@ -1,7 +1,6 @@
 import re
 from django.core.exceptions import ValidationError
-from django.core.validators import RegexValidator
-from django.db import models
+
 
 
 def validate_phone_number(value):
