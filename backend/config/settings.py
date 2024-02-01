673df786fcfe140835b9e28aa62a84c16c5aa246
Klaxer77 <goluzin-vanya@mail.ru>
2024-02-01 22:23:48 +0500

    adwdad

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -186,6 +186,13 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3001',
 ]
 
+#Cookie
+SESSION_COOKIE_HTTPONLY = False
+SESSION_COOKIE_SAMESITE = None
+CSRF_COOKIE_SAMESITE = None
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3001', 'http://127.0.0.1:3000']
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
