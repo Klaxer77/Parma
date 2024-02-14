5ea485b520158e321c41c3e6d0f19e10198c9242
frontend-case <frontend-case@mail.ru>
2024-02-14 22:55:00 +0500

    ы

--- frontend/src/Redux/Profile/ActiveReselve/ActiveReselve.slice.js
+++ frontend/src/Redux/Profile/ActiveReselve/ActiveReselve.slice.js
@@ -1,9 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 
+
 const initialState = {
   infoActiveReselve: [],
-  loading: false
+  numberPlace: null,
+  date_start: null,
+  date_end: null,
+  first_name: null,
+  loading: false,
 }
 
 export const ActiveReselve = createSlice({
@@ -15,10 +20,25 @@ export const ActiveReselve = createSlice({
     },
     setInfoActiveReselve: (state, action) => {
       state.infoActiveReselve.push(action.payload)
-    }
+    },
+    deleteInfoActiveReselve: (state) => {
+      state.infoActiveReselve = []
+    },
+    setNumberPlace: (state, action) => {
+      state.numberPlace = action.payload
+    },
+    setDateStart: (state, action) => {
+      state.date_start = action.payload
+    },
+    setDateEnd: (state, action) => {
+      state.date_end = action.payload
+    },
+    setFirstName: (state, action) => {
+      state.date_end = action.payload
+    },
   },
 })
 
-export const { setLoading, setInfoActiveReselve } = ActiveReselve.actions
+export const { setLoading, setInfoActiveReselve, setNumberPlace, setDateStart, setDateEnd, setFirstName, deleteInfoActiveReselve } = ActiveReselve.actions
 
 export default ActiveReselve.reducer
