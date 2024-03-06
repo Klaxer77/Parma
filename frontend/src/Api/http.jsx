1d6715cea9daa417b8b16b313ac89a9a9455c344
Klaxer77 <goluzin-vanya@mail.ru>
2024-03-06 21:10:53 +0500

    adwdad

--- frontend/src/Api/http.jsx
+++ frontend/src/Api/http.jsx
@@ -7,12 +7,9 @@ export const $profile = axios.create({
 });
 
 export const $login = axios.create({
-<<<<<<< HEAD
   // baseURL: 'http://84.38.183.246/auth/jwt/create/',
   baseURL: 'http://localhost:8000/auth/jwt/create/',
-=======
-  baseURL: 'http://84.38.183.246/auth/jwt',
->>>>>>> c41720402e963e6475dc68c404752a8ebf3d7bd0
+
 });
 
 export const $map = axios.create({
