e538bc0525130398a4b027e28cf36b2eae44425e
Klaxer77 <goluzin-vanya@mail.ru>
2024-03-06 12:48:43 +0500

    adwdad

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -249,11 +249,12 @@ LOGOUT_REDIRECT_URL = '/admin'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
+# STATICFILES_DIRS = (
     
-    BASE_DIR / 'static',
-)
+#     BASE_DIR / 'static',
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
